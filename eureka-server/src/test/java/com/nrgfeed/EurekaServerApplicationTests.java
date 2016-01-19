49cd7d9a2a78cd8761db0158fc7fb202205ad996
Rick Odeen <rodeen@nrgfeed.com>
2016-01-18 18:35:18 -0800

    Initial checkin of Eureka Server

---
+++ eureka-server/src/test/java/com/nrgfeed/EurekaServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nrgfeed;
+
+import com.nrgfeed.application.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+public class EurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
